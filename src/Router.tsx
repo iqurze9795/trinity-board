41a797d6fd0419b5d1bacd4af34f302e39677139
Nick <k.n.nuankaew@gmail.com>
2021-08-07 21:24:49 +0700

    add biswap

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Home } from './pages/home'
 import { Pool } from './pages/pool'
 import { Warden } from './pages/protocol/warden/warden'
+import { Biswap } from './pages/protocol/biswap/biswap'
 import { history } from "./history"
 import { Router, Switch, Route } from "react-router-dom"
 
@@ -13,6 +14,7 @@ class AppRouter extends React.Component {
           <Route exact path="/" component={Pool} />
           <Route exact path="/pool" component={Pool} />
           <Route exact path="/warden" component={Warden} />
+          <Route exact path="/biswap" component={Biswap} />
         </Switch>
       </Router>
     )
